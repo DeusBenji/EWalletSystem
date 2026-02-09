5df8b2d2cfd0b16bb8aeeb48db630d934665c16a
DeusBenji <Benj3249@gmail.com>
2026-02-09 22:17:50 +0100

    Minor adj

--- src/Services/IdentityService/IdentityService.Application/Security/SubIdHasher.cs
+++ src/Services/IdentityService/IdentityService.Application/Security/SubIdHasher.cs
@@ -10,7 +10,7 @@ namespace IdentityService.Application.Security
             using var sha = SHA256.Create();
             var bytes = Encoding.UTF8.GetBytes(subId);
             var hashBytes = sha.ComputeHash(bytes);
-            return Convert.ToHexString(hashBytes);   // returnerer en hex string
+            return Convert.ToHexString(hashBytes);   // returnerer en hex string 
         }
     }
 }
